b663498e9ae0cbb76d3ea39a29c2a75b7c968047
tfussell <thomas.fussell@gmail.com>
2013-04-29 17:01:28 -0300

    Create TransparentWindow.cs

---
+++ TransparentWindow.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace WinDock
+{
+    class TransparentWindow : Form
+    {
+        public TransparentWindow()
+        {
+            // This form should not have a border or else Windows will clip it.
+            FormBorderStyle = FormBorderStyle.None;
+
+            TopMost = true;
+            ShowInTaskbar = false;
+        }
+
+        public void SetBitmap(Bitmap bitmap)
+        {
+            if (bitmap.PixelFormat != PixelFormat.Format32bppArgb)
+            {
+                throw new ApplicationException("The bitmap must be 32ppp with alpha-channel.");
+            }
+
+            IntPtr screenDc = Win32.GetDC(IntPtr.Zero);
+            IntPtr memDc = Win32.CreateCompatibleDC(screenDc);
+            IntPtr hBitmap = IntPtr.Zero;
+            IntPtr oldBitmap = IntPtr.Zero;
+
+            try
+            {
+                hBitmap = bitmap.GetHbitmap(Color.FromArgb(0));  // grab a GDI handle from this GDI+ bitmap
+                oldBitmap = Win32.SelectObject(memDc, hBitmap);
+
+                Win32.Size size = new Win32.Size(bitmap.Width, bitmap.Height);
+                Win32.Point pointSource = new Win32.Point(0, 0);
+                Win32.Point topPos = new Win32.Point(0, 1024 - 200);
+                Win32.BLENDFUNCTION blend = new Win32.BLENDFUNCTION();
+                blend.BlendOp = Win32.AC_SRC_OVER;
+                blend.BlendFlags = 0;
+                blend.SourceConstantAlpha = 255;
+                blend.AlphaFormat = Win32.AC_SRC_ALPHA;
+
+                Win32.UpdateLayeredWindow(Handle, screenDc, ref topPos, ref size, memDc, ref pointSource, 0, ref blend, Win32.ULW_ALPHA);
+            }
+            finally
+            {
+                Win32.ReleaseDC(IntPtr.Zero, screenDc);
+                if (hBitmap != IntPtr.Zero)
+                {
+                    Win32.SelectObject(memDc, oldBitmap);
+                    Win32.DeleteObject(hBitmap);
+                }
+                Win32.DeleteDC(memDc);
+            }
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x00080000; // This form has to have the WS_EX_LAYERED extended style
+                return cp;
+            }
+        }
+    }
+}
