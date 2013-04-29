14d6f35817a9c70a0055172784bbb288540ec1ab
tfussell <thomas.fussell@gmail.com>
2013-04-29 17:04:21 -0300

    Create FloatingDockIcon.cs

---
+++ FloatingDockIcon.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WinDock
+{
+    class FloatingDockIcon : TransparentWindow
+    {
+        public FloatingDockIcon()
+        {
+
+        }
+
+        public Bitmap Bitmap { get; set; }
+    }
+}
