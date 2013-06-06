52a5cb826f21c26864e99ccf1fd1a433ceb10bb4
tfussell <thomas.fussell@gmail.com>
2013-06-06 00:56:11 -0400

    Cleaned up code and added functionality to ApplicationIcon class

--- Items/ApplicationIcon.cs
+++ Items/ApplicationIcon.cs
@@ -1,124 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Drawing;
 using System.Diagnostics;
-using IWshRuntimeLibrary;
-using System.Drawing.Imaging;
+using System.Drawing;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
-namespace WinDock
+namespace AnotherDirect2DTest
 {
-    class ApplicationIcon : DockIcon
+    internal class ApplicationIcon : DockItem
     {
-        Process process;
-        public bool Running
-        {
-            get { return process != null && !process.HasExited; }
-        }
-        public String ExecutablePath { get; set; }
-        private static Bitmap runningIndicatorBitmap;
+        public string DisplayName;
+        public string ExecutablePath;
 
-        static ApplicationIcon()
+        public ApplicationIcon(string filename)
         {
-            runningIndicatorBitmap = (Bitmap)Image.FromFile(Configuration.IconsFolder + Path.DirectorySeparatorChar + Configuration.RunningIndicatorFilename);
-        }
+            ExecutablePath = filename;
 
-        public ApplicationIcon(String path)
-        {
-            ExecutablePath = path;
-
-            if (IsShortcut(path))
+            if (IsShortcut(filename))
             {
-                DisplayName = System.IO.Path.GetFileNameWithoutExtension(path);
-                ExecutablePath = ExtractExecutableFromShortcut(path);
+                DisplayName = Path.GetFileNameWithoutExtension(filename);
+                var referent = Shortcut.ParseShortcut(filename);
+                if (referent != null)
+                    Image = ExtractIconBitmap(referent) ?? ExtractIconBitmap(filename);
             }
 
-            Bitmap = CreateReflection(ExtractIconBitmap(ExecutablePath));
-
-            if (Bitmap == null)
+            if (Image == null)
             {
-                Bitmap = Icon.ExtractAssociatedIcon(ExecutablePath).ToBitmap();
+                var associatedIcon = Icon.ExtractAssociatedIcon(filename);
+                if (associatedIcon != null)
+                    Image = associatedIcon.ToBitmap();
             }
 
-            process = null;
-
-            Width = Configuration.IconSize;
-            Height = Configuration.IconSize * 2;
-        }
-
-        public override void OnClick()
-        {
-            base.OnClick();
-
-            Launch();
-        }
-
-        public override void Paint(Graphics graphics)
-        {
-            base.Paint(graphics);
-
-            if (Running)
-                graphics.DrawImage(runningIndicatorBitmap, X + 17, Y + 51, 10, 10);
-        }
-
-        Bitmap CreateReflection(Bitmap bitmap)
-        {
-            Bitmap reflection = new Bitmap(bitmap.Width, bitmap.Height * 2);
-
-            using (Graphics graphics = Graphics.FromImage(reflection))
+            if (Image != null)
             {
-                graphics.DrawImage(bitmap, new Point(0, 0));
-
-                ColorMatrix matrix = new ColorMatrix();
-                matrix.Matrix33 = 0.3F;
-                ImageAttributes attributes = new ImageAttributes();
-                attributes.SetColorMatrix(matrix, ColorMatrixFlag.Default, ColorAdjustType.Bitmap);
-                bitmap.RotateFlip(RotateFlipType.RotateNoneFlipY);
-
-                graphics.DrawImage(bitmap, new Rectangle(0, bitmap.Height, bitmap.Width, bitmap.Height), 0, 0, bitmap.Width, bitmap.Height, GraphicsUnit.Pixel, attributes);
+                ReflectionImage = CreateReflection(Image);
             }
-
-            return reflection;
         }
 
-        private bool IsShortcut(String executable_path)
+        public override void OnMouseClick(MouseEventArgs e)
         {
-            return executable_path.ToLower().EndsWith(".lnk");
+            Process.Start(ExecutablePath);
         }
 
-        private String ExtractExecutableFromShortcut(String executable_path)
+        private static bool IsShortcut(string filename)
         {
-            WshShell WShell = new WshShell();
-            IWshShortcut link = (IWshShortcut)WShell.CreateShortcut(executable_path);
-
-            return link.TargetPath;
+            return Path.GetExtension(filename.ToLower()) == ".lnk";
         }
 
-        private Bitmap ExtractIconBitmap(string path)
+        private static Bitmap ExtractIconBitmap(string path)
         {
-            // Try for the biggest icon and then work down
-            for (int size = 512; size > 16; size /= 2)
+            // Try for the biggest icon and then work down in powers of two
+            for (var size = 512; size > 16; size /= 2)
             {
-                IntPtr[] phicon = new IntPtr[] { IntPtr.Zero };
-                IntPtr[] piconid = new IntPtr[] { IntPtr.Zero };
+                var phicon = new[] { IntPtr.Zero };
+                var piconid = new[] { IntPtr.Zero };
 
-                Win32.PrivateExtractIcons(path, 0, size, size, phicon, piconid, 1, 0);
+                Api.PrivateExtractIcons(path, 0, size, size, phicon, piconid, 1, 0);
 
                 if (phicon[0] != IntPtr.Zero)
                 {
-                    return System.Drawing.Icon.FromHandle(phicon[0]).ToBitmap();
+                    return Icon.FromHandle(phicon[0]).ToBitmap();
                 }
             }
 
             return null;
         }
 
-        public void Launch()
+        private static class Api
         {
-            if(process == null || process.HasExited)
-                process = Process.Start(ExecutablePath);
+            [DllImport("user32.dll", CharSet = CharSet.Auto)]
+            public static extern UInt32 PrivateExtractIcons(String lpszFile, int nIconIndex, int cxIcon, int cyIcon,
+                                                            IntPtr[] phicon, IntPtr[] piconid, UInt32 nIcons,
+                                                            UInt32 flags);
         }
     }
 }
